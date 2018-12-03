a4c7bb7d8cdea16042b6bee5af08a88940a32336
ritou <ritou.06@gmail.com>
2018-12-04 00:13:45 +0900

    add more test for integer

--- test/cbor_test.exs
+++ test/cbor_test.exs
@@ -11,7 +11,19 @@ defmodule CborTest do
     round_trip(65535)
     round_trip(65536)
     round_trip(4_294_967_295)
+    round_trip(4_294_967_296)
     round_trip(18_446_744_073_709_551_615)
+
+    assert Cbor.encode(0) == <<0>>
+    assert Cbor.encode(23) == <<23>>
+    assert Cbor.encode(24) == <<24, 24>>
+    assert Cbor.encode(255) == <<24, 255>>
+    assert Cbor.encode(256) == <<25, 1, 0>>
+    assert Cbor.encode(65535) == <<25, 255, 255>>
+    assert Cbor.encode(65536) == <<26, 0, 1, 0, 0>>
+    assert Cbor.encode(4_294_967_295) == <<26, 255, 255, 255, 255>>
+    assert Cbor.encode(4_294_967_296) == <<27, 0, 0, 0, 1, 0, 0, 0, 0>>
+    assert Cbor.encode(18_446_744_073_709_551_615) == <<27, 255, 255, 255, 255, 255, 255, 255, 255>>
   end
 
   test "negative integers" do
@@ -24,6 +36,17 @@ defmodule CborTest do
     round_trip(-65537)
     round_trip(-4_294_967_296)
     round_trip(-18_446_744_073_709_551_616)
+
+    assert Cbor.encode(-1) == <<32>>
+    assert Cbor.encode(-24) == <<55>>
+    assert Cbor.encode(-25) == <<56, 24>>
+    assert Cbor.encode(-256) == <<56, 255>>
+    assert Cbor.encode(-257) == <<57, 1, 0>>
+    assert Cbor.encode(-65536) == <<57, 255, 255>>
+    assert Cbor.encode(-65537) == <<58, 0, 1, 0, 0>>
+    assert Cbor.encode(-4_294_967_296) == <<58, 255, 255, 255, 255>>
+    assert Cbor.encode(-4_294_967_297) == <<59, 0, 0, 0, 1, 0, 0, 0, 0>>
+    assert Cbor.encode(-18_446_744_073_709_551_616) == <<59, 255, 255, 255, 255, 255, 255, 255, 255>>
   end
 
   test "strings (symbols)" do
